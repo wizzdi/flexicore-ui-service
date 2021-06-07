c279666057ff1c570873de2e05e363ccbd1fe188
Asaf Ben Natan <asaf@wizzdi.com>
2021-06-07 14:49:04 +0300

    updated dependencies , added dynamic properties for preset

--- src/main/java/com/flexicore/ui/service/PresetService.java
+++ src/main/java/com/flexicore/ui/service/PresetService.java
@@ -2,101 +2,101 @@ package com.flexicore.ui.service;
 
 import com.flexicore.annotations.plugins.PluginInfo;
 import com.flexicore.data.jsoncontainers.PaginationResponse;
-import com.flexicore.events.BaseclassCreated;
 import com.flexicore.interfaces.ServicePlugin;
 import com.flexicore.model.Baseclass;
-import com.flexicore.model.PermissionGroupToBaseclass;
 import com.flexicore.security.SecurityContext;
-import com.flexicore.service.BaseclassNewService;
+import com.flexicore.service.impl.BaseclassNewService;
 import com.flexicore.ui.data.PresetRepository;
 import com.flexicore.ui.model.Preset;
-import com.flexicore.ui.model.UiField;
 import com.flexicore.ui.request.PresetCreate;
 import com.flexicore.ui.request.PresetFiltering;
 import com.flexicore.ui.request.PresetUpdate;
+import org.pf4j.Extension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
-import org.pf4j.Extension;
-import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Component;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @PluginInfo(version = 1)
 @Extension
 @Component
 public class PresetService implements ServicePlugin {
 
-	@Autowired
-	private Logger logger;
+    @Autowired
+    private Logger logger;
 
-	@PluginInfo(version = 1)
-	@Autowired
-	private PresetRepository presetRepository;
+    @PluginInfo(version = 1)
+    @Autowired
+    private PresetRepository presetRepository;
 
-	@Autowired
-	private BaseclassNewService baseclassNewService;
+    @Autowired
+    private BaseclassNewService baseclassNewService;
 
-	public Preset updatePreset(PresetUpdate updatePreset,
-							   SecurityContext securityContext) {
-		if (updatePresetNoMerge(updatePreset, updatePreset.getPreset())) {
-			presetRepository.merge(updatePreset.getPreset());
-		}
-		return updatePreset.getPreset();
-	}
-
-	public boolean updatePresetNoMerge(PresetCreate createPreset, Preset preset) {
-		boolean update = baseclassNewService.updateBaseclassNoMerge(createPreset,preset);
-
-		if (createPreset.getExternalId() != null && (preset.getExternalId() == null || !createPreset.getExternalId().equals(preset.getExternalId()))) {
-			preset.setExternalId(createPreset.getExternalId());
-
-			update = true;
-		}
-
-		return update;
-	}
-
-	public <T extends Baseclass> T getByIdOrNull(String id, Class<T> c, List<String> batchString, SecurityContext securityContext) {
-		return presetRepository.getByIdOrNull(id, c, batchString, securityContext);
-	}
-
-	public PaginationResponse<Preset> getAllPresets(
-			PresetFiltering presetFiltering, SecurityContext securityContext) {
-		List<Preset> list = listAllPresets(presetFiltering, securityContext);
-		long count = presetRepository.countAllPresets(presetFiltering, securityContext);
-		return new PaginationResponse<>(list, presetFiltering, count);
-	}
-
-	public List<Preset> listAllPresets(PresetFiltering presetFiltering,
-			SecurityContext securityContext) {
-		return presetRepository.listAllPresets(presetFiltering, securityContext);
-	}
-
-	public Preset createPreset(PresetCreate createPreset,
+    public Preset updatePreset(PresetUpdate updatePreset,
                                SecurityContext securityContext) {
-		Preset preset = createPresetNoMerge(createPreset, securityContext);
-		presetRepository.merge(preset);
-		return preset;
+        if (updatePresetNoMerge(updatePreset, updatePreset.getPreset())) {
+            presetRepository.merge(updatePreset.getPreset());
+        }
+        return updatePreset.getPreset();
+    }
 
-	}
+    public boolean updatePresetNoMerge(PresetCreate createPreset, Preset preset) {
+        boolean update = baseclassNewService.updateBaseclassNoMerge(createPreset, preset);
 
+        if (createPreset.getExternalId() != null && (preset.getExternalId() == null || !createPreset.getExternalId().equals(preset.getExternalId()))) {
+            preset.setExternalId(createPreset.getExternalId());
 
+            update = true;
+        }
+        Map<String, Object> map = baseclassNewService.updateDynamic(createPreset.any(), preset.any());
+        if (map != null) {
+            preset.setJsonNode(map);
+            update = true;
+        }
 
-	private Preset createPresetNoMerge(PresetCreate createPreset,
+        return update;
+    }
+
+    public <T extends Baseclass> T getByIdOrNull(String id, Class<T> c, List<String> batchString, SecurityContext securityContext) {
+        return presetRepository.getByIdOrNull(id, c, batchString, securityContext);
+    }
+
+    public PaginationResponse<Preset> getAllPresets(
+            PresetFiltering presetFiltering, SecurityContext securityContext) {
+        List<Preset> list = listAllPresets(presetFiltering, securityContext);
+        long count = presetRepository.countAllPresets(presetFiltering, securityContext);
+        return new PaginationResponse<>(list, presetFiltering, count);
+    }
+
+    public List<Preset> listAllPresets(PresetFiltering presetFiltering,
                                        SecurityContext securityContext) {
-		Preset preset = new Preset(createPreset.getName(), securityContext);
-		updatePresetNoMerge(createPreset, preset);
-		return preset;
-	}
+        return presetRepository.listAllPresets(presetFiltering, securityContext);
+    }
 
-	public void validate(PresetCreate presetCreate,SecurityContext securityContext){
-		baseclassNewService.validateCreate(presetCreate,securityContext);
-	}
+    public Preset createPreset(PresetCreate createPreset,
+                               SecurityContext securityContext) {
+        Preset preset = createPresetNoMerge(createPreset, securityContext);
+        presetRepository.merge(preset);
+        return preset;
 
-	public void validate(PresetFiltering presetFiltering,SecurityContext securityContext){
-		baseclassNewService.validateFilter(presetFiltering,securityContext);
-	}
+    }
+
+
+    private Preset createPresetNoMerge(PresetCreate createPreset,
+                                       SecurityContext securityContext) {
+        Preset preset = new Preset(createPreset.getName(), securityContext);
+        updatePresetNoMerge(createPreset, preset);
+        return preset;
+    }
+
+    public void validate(PresetCreate presetCreate, SecurityContext securityContext) {
+        baseclassNewService.validateCreate(presetCreate, securityContext);
+    }
+
+    public void validate(PresetFiltering presetFiltering, SecurityContext securityContext) {
+        baseclassNewService.validateFilter(presetFiltering, securityContext);
+    }
 
 }
